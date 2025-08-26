9e6a9f8a944e660f6f0fe949ab85a067b3481dc9
Ferdinand Hriadel <fhriadel@Ferdinands-MacBook-Pro.local>
2025-08-26 16:14:56 +0200

    regular button, input remake

--- inputs.js
+++ inputs.js
@@ -1,21 +1,20 @@
 const fileInputEl = document.querySelector('.input-file');
 const textEl = document.querySelector('.file-input-text');
-const labelEl = document.querySelector('.file-input-label');
 
 
 
 fileInputEl?.addEventListener('change', (event) => {
     if (!event?.target?.files.length) {
-        textEl.textContent = 'Choose a file';
+        textEl.textContent = 'Choose a file...';
         textEl.style.color = 'var(--color-dark)';
         textEl.style.left = '0.5rem';
-        labelEl.style.display = 'none';
         return;
     }
-    const file = event?.target?.files[0];
-    textEl.textContent = file.name;
-    textEl.style.color = 'var(--color-mid)';
-    textEl.style.left = '1rem';
-    labelEl.style.display = 'block';
+    else {
+        const file = event?.target?.files[0];
+        textEl.textContent = file.name;
+        textEl.style.color = 'var(--color-mid)';
+        textEl.style.left = '1rem';
+    }
 
 });
