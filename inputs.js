7fa3a6735dd06175bee3e3591f281f79a047254b
Ferdinand Hriadel <fhriadel@Ferdinands-MacBook-Pro.local>
2025-08-26 12:55:15 +0200

    file input

---
+++ inputs.js
@@ -0,0 +1,21 @@
+const fileInputEl = document.querySelector('.input-file');
+const textEl = document.querySelector('.file-input-text');
+const labelEl = document.querySelector('.file-input-label');
+
+
+
+fileInputEl?.addEventListener('change', (event) => {
+    if (!event?.target?.files.length) {
+        textEl.textContent = 'Choose a file';
+        textEl.style.color = 'var(--color-dark)';
+        textEl.style.left = '0.5rem';
+        labelEl.style.display = 'none';
+        return;
+    }
+    const file = event?.target?.files[0];
+    textEl.textContent = file.name;
+    textEl.style.color = 'var(--color-mid)';
+    textEl.style.left = '1rem';
+    labelEl.style.display = 'block';
+
+});
