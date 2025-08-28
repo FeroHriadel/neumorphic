cbf1ad729a81c1f0a3b6f43e1df01397999d5f81
Ferdinand Hriadel <fhriadel@Ferdinands-MacBook-Pro.local>
2025-08-28 16:55:27 +0200

    extended globals, file inputs overflow fix

--- inputs.js
+++ inputs.js
@@ -1,20 +1,37 @@
+// File Input
 const fileInputEl = document.querySelector('.input-file');
-const textEl = document.querySelector('.file-input-text');
+const fileInputText = document.querySelector('.file-input-text');
 
 
 
 fileInputEl?.addEventListener('change', (event) => {
-    if (!event?.target?.files.length) {
-        textEl.textContent = 'Choose a file...';
-        textEl.style.color = 'var(--color-dark)';
-        textEl.style.left = '0.5rem';
+    if (!event?.target?.files?.length) {
+        fileInputText.textContent = 'Choose a file...';
+        fileInputText.style.color = 'var(--color-dark)';
+        fileInputText.style.left = '0.5rem';
         return;
     }
     else {
         const file = event?.target?.files[0];
-        textEl.textContent = file.name;
-        textEl.style.color = 'var(--color-mid)';
-        textEl.style.left = '1rem';
+        fileInputText.textContent = file.name;
+        fileInputText.style.color = 'var(--color-mid)';
+        fileInputText.style.left = '1rem';
     }
+});
 
+
+
+// Image Input
+const imageInputEl = document.querySelector('.image-input');
+const imageInputText = document.querySelector('.image-input-text');
+
+imageInputEl?.addEventListener('change', (event) => {
+    if (!event?.target?.files?.length) {
+        imageInputText.textContent = 'Choose an image';
+    }
+    else {
+        const file = event?.target?.files[0];
+        imageInputText.textContent = file.name;
+    }
+ 
 });
